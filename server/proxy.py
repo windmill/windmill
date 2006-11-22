e22cb341159e319e969eace4b1ccc33bfa5f3702
Mikeal Rogers <mikeal.rogers@gmail.com>
2006-11-22 06:42:57 +0000

    Modularized the different apps into callable classes. This is in anticipation of needing to instantiate the different objects one at a time and pass them into each other, like the object that tells json what to do next from the last xmlrpc request.
    -Mikeal
    
    
    
    git-svn-id: http://svn.getwindmill.com/trunk@7 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- server/proxy.py
+++ server/proxy.py
@@ -18,28 +18,18 @@ from urlparse import urlparse, urljoin
 import os.path
 import httplib, copy
 
-CORE_PATH = os.path.abspath('../core')
+CORE_PATH = os.path.abspath('../')
 PORT = 4444
 
 # wsgiref.utils.is_hop_by_hop doesn't pick up proxy-connection so we need to write our own
-
 _hoppish = {
     'connection':1, 'keep-alive':1, 'proxy-authenticate':1,
     'proxy-authorization':1, 'te':1, 'trailers':1, 'transfer-encoding':1,
     'upgrade':1, 'proxy-connection':1 }
     
-    
 def is_hop_by_hop(header):
     return _hoppish.has_key(header.lower())
         
-
-def guess_content_type(path_info):
-    if path_info.endswith('.js'):
-        return 'application/x-javascript'
-    elif path_info.endswith('.html'):
-        return 'text/html'
-    else:
-        return 'text/plain'
 
 class WindmillServApplication(object):
     """Application to serve out windmill provided"""
@@ -53,95 +43,140 @@ class WindmillServApplication(object):
         serve_file = url[2].replace('/windmill-serv/', '')
         #Open file
         f = open('%s/%s' % (CORE_PATH, serve_file), 'r')
-        content_type = guess_content_type(environ['PATH_INFO'])
+        content_type = self.guess_content_type(environ['PATH_INFO'])
         start_response('200 OK', [('Cache-Control','no-cache'), ('Pragma','no-cache'),
                                   ('Content-Type', content_type)])
         return [f.read()]
-            
-    __call__ = handler
-    
-    
-def windmill_jsonrpc_app(environ, start_response):
-    """JSONRPC service for windmill browser core to communicate with"""
-    
-    start_response("200 OK", [('Content-Type','text/plain')])
-    return ['']
-    
-    
-def windmill_proxy(environ, start_response):
-    """Proxy for requests to the actual http server"""
-    if environ['QUERY_STRING'] != '':
-        request = environ['PATH_INFO']+'?'+environ['QUERY_STRING']
-    else:
-        request = environ['PATH_INFO']
 
-    url = urlparse(request)
+    def guess_content_type(self, path_info):
+        if path_info.endswith('.js'):
+            return 'application/x-javascript'
+        elif path_info.endswith('.html'):
+            return 'text/html'
+        else:
+            return 'text/plain'
+            
+    def __call__(self, environ, start_response):
+        return self.handler(environ, start_response)            
+                
+class WindmillJSONRPCApplication(object):
+    """Application to handle requests to the JSONRPC service"""
     
-    # Create connection object
-    if url[0] == 'http':
+    def handler(self, environ, start_response):
+        """JSONRPC service for windmill browser core to communicate with"""
+        
+        start_response("200 OK", [('Content-Type','text/plain')])
+        return ['']
+    
+    def __call__(self, environ, start_response):
+        return self.handler(environ, start_response)    
+
+class WindmillXMLRPCApplication(object):
+    """Application to handle requests to the XMLRPC service"""
+
+    def handler(self, environ, start_response):
+        """XMLRPC service for windmill browser core to communicate with"""
+
+        start_response("200 OK", [('Content-Type','text/plain')])
+        return ['']
+
+    def __call__(self, environ, start_response):
+        return self.handler(environ, start_response)    
+
+
+class WindmillProxyApplication(object):
+    """Application to handle requests that need to be proxied"""
+    
+    def handler(self, environ, start_response):
+        """Proxy for requests to the actual http server"""
+        if environ['QUERY_STRING'] != '':
+            request = environ['PATH_INFO']+'?'+environ['QUERY_STRING']
+        else:
+            request = environ['PATH_INFO']
+
+        url = urlparse(request)
+    
+        # Create connection object
+        if url[0] == 'http':
+            try:
+                connection = httplib.HTTPConnection(url[1])
+                # Build path
+                path = request.replace('http://%s' % url[1], '')
+            except:
+                start_response("501 Gateway error", [('Content-Type', 'text/html')])
+                return ['<H1>Could not connect</H1>']
+        else:
+            # We don't currently support SSL or any other scheme
+            start_response("501 Gateway error", [('Content-Type', 'text/html')])
+            return ['<H1>Gateway does not support scheme</H1>']
+            
+        # Read in request body if it exists    
+        body = None
+        if environ.get('CONTENT_LENGTH'):
+            length = int(environ['CONTENT_LENGTH'])
+            body = environ['wsgi.input'].read(length)
+        
+        # Build headers
+        headers = {}
+        for key in environ.keys():
+            # Keys that start with HTTP_ are all headers
+            if key.startswith('HTTP_'):
+                # This is a hacky way of getting the header names right
+                value = environ[key]
+                key = key.replace('HTTP_', '').swapcase().replace('_', '-')
+                if is_hop_by_hop(key) is False:
+                    headers[key] = value
+    
+        # Add our host if one isn't defined
+        if not headers.has_key('host'):
+            headers['host'] = environ['SERVER_NAME']   
+    
+        # Make the remote request
         try:
-            connection = httplib.HTTPConnection(url[1])
-            # Build path
-            path = request.replace('http://%s' % url[1], '')
+            connection.request(environ['REQUEST_METHOD'], path, body=body, headers=headers)
         except:
+            # We need exception handling in the case the server fails, it's an edge case but I've seen it
             start_response("501 Gateway error", [('Content-Type', 'text/html')])
             return ['<H1>Could not connect</H1>']
-    else:
-        # We don't currently support SSL or any other scheme
-        start_response("501 Gateway error", [('Content-Type', 'text/html')])
-        return ['<H1>Gateway does not support scheme</H1>']
-            
-    # Read in request body if it exists    
-    body = None
-    if environ.get('CONTENT_LENGTH'):
-        length = int(environ['CONTENT_LENGTH'])
-        body = environ['wsgi.input'].read(length)
+
+        response = connection.getresponse()
+    
+        # Return the proper wsgi response
+        response_body = response.read()
+        hopped_headers = response.getheaders()
+        headers = copy.copy(hopped_headers)
+        for header in hopped_headers:
+            if is_hop_by_hop(header[0]):
+                headers.remove(header)
+        start_response(response.status.__str__()+' '+response.reason, headers)
+        return [response_body]
+    
+    def __call__(self, environ, start_response):
+        return self.handler(environ, start_response)
+    
+class WindmillChooserApplication(object):
+    """Application to handle choosing the proper application to handle each request"""
+    def __init__(self, windmill_serv_app, windmill_jsonrpc_app, windmill_xmlrpc_app, windmill_proxy_app):
+        self.windmill_serv_app = windmill_serv_app
+        self.windmill_jsonrpc_app = windmill_jsonrpc_app
+        self.windmill_xmlrpc_app = windmill_xmlrpc_app
+        self.windmill_proxy_app = windmill_proxy_app
+
+    def handler(self, environ, start_response):
+        """Windmill app chooser"""
         
-    # Build headers
-    headers = {}
-    for key in environ.keys():
-        # Keys that start with HTTP_ are all headers
-        if key.startswith('HTTP_'):
-            # This is a hacky way of getting the header names right
-            value = environ[key]
-            key = key.replace('HTTP_', '').swapcase().replace('_', '-')
-            if is_hop_by_hop(key) is False:
-                headers[key] = value
-    
-    # Add our host if one isn't defined
-    if not headers.has_key('host'):
-        headers['host'] = environ['SERVER_NAME']   
-    
-    # Make the remote request
-    try:
-        connection.request(environ['REQUEST_METHOD'], path, body=body, headers=headers)
-    except:
-        # We need exception handling in the case the server fails, it's an edge case but I've seen it
-        start_response("501 Gateway error", [('Content-Type', 'text/html')])
-        return ['<H1>Could not connect</H1>']
-
-    response = connection.getresponse()
-    
-    # Return the proper wsgi response
-    response_body = response.read()
-    hopped_headers = response.getheaders()
-    headers = copy.copy(hopped_headers)
-    for header in hopped_headers:
-        if is_hop_by_hop(header[0]):
-            headers.remove(header)
-    start_response(response.status.__str__()+' '+response.reason, headers)
-    return [response_body]
-    
-
-def windmill_app_chooser(environ, start_response):
-    """Windmill app chooser"""
-    print environ['PATH_INFO']
-    if environ['PATH_INFO'].find('/windmill-serv/') is not -1:
-        return windmill_serv_app(environ, start_response)
-    elif environ['PATH_INFO'].find('/windmill-jsonrpc/') is not -1:
-        return windmill_jsonrpc_app(environ, start_response)
-    else:
-        return windmill_proxy(environ, start_response)
+        print environ['PATH_INFO']
+        if environ['PATH_INFO'].find('/windmill-serv/') is not -1:
+            return self.windmill_serv_app(environ, start_response)
+        elif environ['PATH_INFO'].find('/windmill-jsonrpc/') is not -1:
+            return self.windmill_jsonrpc_app(environ, start_response)
+        elif environ['PATH_INFO'].find('/windmill-xmlrpc/') is not -1:
+            return self.windmill_xmlrpc_app(environ, start_response)
+        else:
+            return self.windmill_proxy_app(environ, start_response)
+            
+    def __call__(self, environ, start_response):
+        return self.handler(environ, start_response)
 
 
 import SocketServer
@@ -180,9 +215,18 @@ class ThreadedWSGIServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
     def set_app(self,application):
         self.application = application
+        
+def make_windmill_server(port=PORT, core_path=CORE_PATH):
+    windmill_serv_app = WindmillServApplication(core_path)
+    windmill_proxy_app = WindmillProxyApplication()
+    windmill_jsonrpc_app = WindmillJSONRPCApplication()
+    windmill_xmlrpc_app =  WindmillXMLRPCApplication()
+    windmill_chooser_app = WindmillChooserApplication(windmill_serv_app, windmill_jsonrpc_app,
+                                                      windmill_xmlrpc_app, windmill_proxy_app)
+    return make_server('', port, windmill_chooser_app, server_class=ThreadedWSGIServer)
 
 def main(port=PORT):
-    httpd = make_server('', port, windmill_app_chooser, server_class=ThreadedWSGIServer)
+    httpd = make_windmill_server()
     print "Serving HTTP on port %s..." % port
 
     # Respond to requests until process is killed
