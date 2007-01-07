88fc3ed288cf58e4cf051605b4ca38a023e2a6c5
Mikeal Rogers <mikeal.rogers@gmail.com>
2007-01-07 03:42:41 +0000

    Updating copyright.
    git-svn-id: http://svn.getwindmill.com/trunk@65 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- contrib/pyselenium/src/__init__.py
+++ contrib/pyselenium/src/__init__.py
@@ -1,5 +1,5 @@
 #   Copyright 2006 ThoughtWorks, Inc.
-#   Copyright (c) 2006 Open Source Applications Foundation
+#   Copyright (c) 2006-2007Open Source Applications Foundation
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
