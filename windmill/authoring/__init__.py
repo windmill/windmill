b17778d7609defe7ac173e84e736ea0f595f3598
Mikeal Rogers <mikeal.rogers@gmail.com>
2007-05-25 19:04:08 +0000

    Finished seperation of non-windmill functionality in to seperate modules.
    Everyone should python setup.py develop.
    
    -Mikeal
    
    
    
    git-svn-id: http://svn.getwindmill.com/trunk@195 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- windmill/authoring/__init__.py
+++ windmill/authoring/__init__.py
@@ -23,48 +23,49 @@ class Controller(object):
     method_proxy = xmlrpclib.ServerProxy(RPC_URL)
     _enable_unittest = False
     enable_assertions = True
-    
-    def __init__(self):
-        """Assign all available attributes to instance so they are easily introspected"""        
-        class ExecuteOnController(object):
-            def __init__(self, name, execution_method, command_exec_method):
-                self.name = name
-                if name.find('command') is not -1:
-                    self.execution_method = command_execution_method
-                else:
-                    self.execution_method = execution_method
-                self.command_exec_method
-            def __call__(self, **kwargs):
-                rpc = {'method':self.name, 'params':kwargs}
-                self.execution_method(simplejson.dumps(rpc))
-                
-        for attribute in self.method_proxy.getControllerMethods():
-            if attribute.find('.') is -1:
-                self.__dict__[attribute] = ExecuteOnController(attribute, self._exec_controller_test, self._exec_controller_command)
-            else:
-                
-    
-    def enable_unittest(self, unittest_cls_inst):
-        self._unittest_cls_inst = cls_inst
-    
-    def _exec_controller_test(self, name, **kwargs):
-        result = getattr(self.method_proxy, name)(**kwargs)
-        if self._enable_unittest:
-            self._unittest_cls_inst.assertEqual(result, True)
-        if self.enable_assertions:
-            assert result is True
-        return result
-    
-    def _exec_controller_command(self, name, **kwargs):
-        result = getattr(self.method_proxy, name)(**kwargs)
-        if self._enable_unittest:
-            self._unittest_cls_inst.failUnless(result, 'remote call did not return proper result')
-        if self.enable_assertions:
-            assert result is not False
-            assert result is not ''
-            assert result is not None
-        return result
-            
+        # 
+        # def __init__(self):
+        #     """Assign all available attributes to instance so they are easily introspected"""        
+        #     class ExecuteOnController(object):
+        #         def __init__(self, name, execution_method, command_exec_method):
+        #             self.name = name
+        #             if name.find('command') is not -1:
+        #                 self.execution_method = command_execution_method
+        #             else:
+        #                 self.execution_method = execution_method
+        #             self.command_exec_method
+        #         def __call__(self, **kwargs):
+        #             rpc = {'method':self.name, 'params':kwargs}
+        #             self.execution_method(simplejson.dumps(rpc))
+        #             
+        #     for attribute in self.method_proxy.getControllerMethods():
+        #         if attribute.find('.') is -1:
+        #             self.__dict__[attribute] = ExecuteOnController(attribute, self._exec_controller_test, self._exec_controller_command)
+        #         else:
+        #             pass
+        #             
+        # 
+        # def enable_unittest(self, unittest_cls_inst):
+        #     self._unittest_cls_inst = cls_inst
+        # 
+        # def _exec_controller_test(self, name, **kwargs):
+        #     result = getattr(self.method_proxy, name)(**kwargs)
+        #     if self._enable_unittest:
+        #         self._unittest_cls_inst.assertEqual(result, True)
+        #     if self.enable_assertions:
+        #         assert result is True
+        #     return result
+        # 
+        # def _exec_controller_command(self, name, **kwargs):
+        #     result = getattr(self.method_proxy, name)(**kwargs)
+        #     if self._enable_unittest:
+        #         self._unittest_cls_inst.failUnless(result, 'remote call did not return proper result')
+        #     if self.enable_assertions:
+        #         assert result is not False
+        #         assert result is not ''
+        #         assert result is not None
+        #     return result
+        #         
             
     
         
--- windmill/server/wsgi.py
+++ windmill/server/wsgi.py
@@ -12,15 +12,22 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from urlparse import urlparse, urljoin
-import httplib, os.path, copy, time, socket, logging, sys, traceback, random
-from StringIO import StringIO
+## Note: This file is very trim now because we've broken wsgi_fileserver, wsgi_proxy, wsgi_jsonrpc, and wsgi_xmlrpc 
+## in to their own libraries which are now distributed on their own and treated as dependencies 
 
-import jsonrpc, logger
-import windmill
-
+import httplib
+import copy
+import socket 
+import random
+from urlparse import urlparse
 import logging
-logger = logging.getLogger('server.wsgi')
+logger = logging.getLogger(__name__)
+
+import windmill
+import wsgi_jsonrpc
+import wsgi_xmlrpc
+import wsgi_proxy
+import wsgi_fileserver
 
 PORT = 4444
 
@@ -28,163 +35,27 @@ START_DST_PORT = 32000
 CURRENT_DST_PORT = [random.randint(32000, 34000)]
 
 LOCAL_IP_ADDRESS = socket.gethostbyname(socket.gethostname())
-
-# wsgiref.utils.is_hop_by_hop doesn't pick up proxy-connection so we need to write our own
-_hoppish = {
-    'connection':1, 'keep-alive':1, 'proxy-authenticate':1,
-    'proxy-authorization':1, 'te':1, 'trailers':1, 'transfer-encoding':1,
-    'upgrade':1, 'proxy-connection':1 }
-    
-def is_hop_by_hop(header):
-    return _hoppish.has_key(header.lower())
     
 def reconstruct_url(environ):
     # From WSGI spec, PEP 333
     from urllib import quote
     url = environ['wsgi.url_scheme']+'://'
-
-    if environ.get('HTTP_HOST'):
-        url += environ['HTTP_HOST']
+    if environ.get('HTTP_HOST'): url += environ['HTTP_HOST']
     else:
         url += environ['SERVER_NAME']
-
         if environ['wsgi.url_scheme'] == 'https':
             if environ['SERVER_PORT'] != '443':
                url += ':' + environ['SERVER_PORT']
         else:
             if environ['SERVER_PORT'] != '80':
                url += ':' + environ['SERVER_PORT']
-
     url += quote(environ.get('SCRIPT_NAME',''))
-    url += quote(environ.get('PATH_INFO',''))
+    url += quote(environ.get('PATH_INFO','')).replace(url.replace(':', '%3A'), '')
     if environ.get('QUERY_STRING'):
         url += '?' + environ['QUERY_STRING']
-    
     environ['reconstructed_url'] = url
-        
     return url
         
-
-class WindmillServApplication(object):
-    """Application to serve out windmill provided"""
-    
-    def __init__(self, logger, js_path):
-        self.path = js_path
-        self.logger = logger
-    
-    def handler(self, environ, start_response):
-        """Application to serve out windmill provided"""
-        url = urlparse(reconstruct_url(environ))
-        split_url = url.path.split('/windmill-serv/')
-        serve_file = split_url[1]
-        
-        #Open file
-        try:
-            f = open('%s/%s' % (self.path, serve_file), 'r')
-            self.logger.debug('opened file %s' % serve_file)
-        except:
-            self.logger.error('failed to open file %s/%s' % (self.path, serve_file))
-            start_response('404 Not found', [('Content-Type', 'text/plain')])
-            return ['404 Not Found']
-        content_type = self.guess_content_type(environ['PATH_INFO'])
-        start_response('200 OK', [('Cache-Control','no-cache'), ('Pragma','no-cache'),
-                                  ('Content-Type', content_type)])
-        return [f.read()]
-
-    def guess_content_type(self, path_info):
-        if path_info.endswith('.js'):
-            return 'application/x-javascript'
-        elif path_info.endswith('.html'):
-            return 'text/html'
-        elif path_info.endswith('.css'):
-            return 'text/css'
-        else:
-            return 'text/plain'
-            
-    def __call__(self, environ, start_response):
-        return self.handler(environ, start_response)            
-        
-                
-class WindmillJSONRPCApplication(object):
-    """Application to handle requests to the JSONRPC service"""
-    
-    def __init__(self, json_methods_instance, logger):
-        """Create windmill jsonrpc dispatcher"""
-        self.jsonrpc_dispatcher = jsonrpc.WSGIJSONRPCDispatcher(instance=json_methods_instance)
-        self.logger = logger
-    
-    def handler(self, environ, start_response):
-        """JSONRPC service for windmill browser core to communicate with"""
-        
-        return self.jsonrpc_dispatcher.handler(environ, start_response)
-    
-    def __call__(self, environ, start_response):
-        return self.handler(environ, start_response)    
-        
-
-class WindmillXMLRPCApplication(object):
-    """Application to handle requests to the XMLRPC service"""
-
-    def __init__(self, xmlrpc_methods_instance, logger):
-        """Create windmill xmlrpc dispatcher"""
-        from SimpleXMLRPCServer import SimpleXMLRPCDispatcher  
-        self.dispatcher = SimpleXMLRPCDispatcher(allow_none=False, encoding=None)
-        self.dispatcher.register_instance(xmlrpc_methods_instance)
-        self.dispatcher.register_introspection_functions()
-        self.logger = logger
-
-    def handler(self, environ, start_response):
-        """XMLRPC service for windmill browser core to communicate with"""
-
-        if environ['REQUEST_METHOD'] == 'POST':
-            return self.handle_POST(environ, start_response)
-        else:
-            start_response("400 Bad request", [('Content-Type','text/plain')])
-            return ['']
-        
-    def handle_POST(self, environ, start_response):
-        """Handles the HTTP POST request.
-
-        Attempts to interpret all HTTP POST requests as XML-RPC calls,
-        which are forwarded to the server's _dispatch method for handling.
-        
-        Most code taken from SimpleXMLRPCServer with modifications for wsgi and my custom dispatcher.
-        """
-        
-        try:
-            # Get arguments by reading body of request.
-            # We read this in chunks to avoid straining
-            # socket.read(); around the 10 or 15Mb mark, some platforms
-            # begin to have problems (bug #792570).
-
-            length = int(environ['CONTENT_LENGTH'])
-            data = environ['wsgi.input'].read(length)
-            
-            max_chunk_size = 10*1024*1024
-            size_remaining = length
-
-            # In previous versions of SimpleXMLRPCServer, _dispatch
-            # could be overridden in this class, instead of in
-            # SimpleXMLRPCDispatcher. To maintain backwards compatibility,
-            # check to see if a subclass implements _dispatch and 
-            # using that method if present.
-            response = self.dispatcher._marshaled_dispatch(
-                    data, getattr(self.dispatcher, '_dispatch', None)
-                )
-        except: # This should only happen if the module is buggy
-            # internal error, report as HTTP server error
-            start_response("500 Server error", [('Content-Type', 'text/plain')])
-            return []
-        else:
-            # got a valid XML RPC response
-            start_response("200 OK", [('Content-Type','text/xml')])
-            return [response]
-            
-
-    def __call__(self, environ, start_response):
-        return self.handler(environ, start_response)    
-                        
-                
 class HTTPConnection(httplib.HTTPConnection):
     
     def connect(self):
@@ -217,99 +88,17 @@ class HTTPConnection(httplib.HTTPConnection):
         if self.sock is not None:
             self.sock.close()
             
+WindmillProxyApplication = wsgi_proxy.WSGIProxyApplication
+WindmillProxyApplication.ConnectionClass = HTTPConnection
             
-class WindmillProxyApplication(object):
-    """Application to handle requests that need to be proxied"""
-    
-    def __init__(self, logger):
-        self.logger = logger
-        
-    ConnectionClass = HTTPConnection
-    
-    def handler(self, environ, start_response):
-        """Proxy for requests to the actual http server"""
-        url = urlparse(environ['reconstructed_url'])
-    
-        # Create connection object
-        try:
-            connection = self.ConnectionClass(url.netloc)
-            # Build path
-            path = url.geturl().replace('%s://%s' % (url.scheme, url.netloc), '')
-        except Exception, e:
-            start_response("501 Gateway error", [('Content-Type', 'text/html')])
-            self.logger.exception('Could not Connect')
-            return ['<H1>Could not connect</H1>']
-
-            
-        # Read in request body if it exists    
-        body = None
-        if environ.get('CONTENT_LENGTH'):
-            length = int(environ['CONTENT_LENGTH'])
-            body = environ['wsgi.input'].read(length)
-            
-            
-        # Build headers
-        headers = {}
-        self.logger.debug('Environ ; %s' % str(environ))
-        for key in environ.keys():
-            # Keys that start with HTTP_ are all headers
-            if key.startswith('HTTP_'):
-                # This is a hacky way of getting the header names right
-                value = environ[key]
-                key = key.replace('HTTP_', '', 1).swapcase().replace('_', '-')
-                if is_hop_by_hop(key) is False:
-                    headers[key] = value
-    
-        # Handler headers that aren't HTTP_ in environ
-        if environ.get('CONTENT_TYPE'):
-            headers['content-type'] = environ['CONTENT_TYPE']
-        
-        # Add our host if one isn't defined
-        if not headers.has_key('host'):
-            headers['host'] = environ['SERVER_NAME']   
-    
-        # Make the remote request
-        try:
-            self.logger.debug('%s %s %s' % (environ['REQUEST_METHOD'], path, str(headers)))
-            connection.request(environ['REQUEST_METHOD'], path, body=body, headers=headers)
-        except:
-            # We need exception handling in the case the server fails, it's an edge case but I've seen it
-            start_response("501 Gateway error", [('Content-Type', 'text/html')])
-            self.logger.exception('Could not Connect')
-            return ['<H1>Could not connect</H1>']
-
-        response = connection.getresponse()
-
-        hopped_headers = response.getheaders()
-        headers = copy.copy(hopped_headers)
-        for header in hopped_headers:
-            if is_hop_by_hop(header[0]):
-                headers.remove(header)
-        
-        start_response(response.status.__str__()+' '+response.reason, headers)
-    
-    
-        # Return the proper wsgi response
-        # response_body = response.read()
-        # 737019
-        return [response.read(response.length)]
-        
-    
-    def __call__(self, environ, start_response):
-        return self.handler(environ, start_response)
-    
-class WindmillSSLProxyApplication(WindmillProxyApplication):
-    
-    ConnectionClass = httplib.HTTPSConnection
-    
+                
 class WindmillChooserApplication(object):
     """Application to handle choosing the proper application to handle each request"""
-    def __init__(self, windmill_serv_app, windmill_jsonrpc_app, windmill_xmlrpc_app, windmill_proxy_app, logger):
+    def __init__(self, windmill_serv_app, windmill_jsonrpc_app, windmill_xmlrpc_app, windmill_proxy_app):
         self.windmill_serv_app = windmill_serv_app
         self.windmill_jsonrpc_app = windmill_jsonrpc_app
         self.windmill_xmlrpc_app = windmill_xmlrpc_app
         self.windmill_proxy_app = windmill_proxy_app
-        self.logger = logger
 
     def handler(self, environ, start_response):
         """Windmill app chooser"""
@@ -317,16 +106,16 @@ class WindmillChooserApplication(object):
         reconstruct_url(environ)
         
         if environ['PATH_INFO'].find('/windmill-serv/') is not -1:
-            self.logger.debug('dispatching request %s to WindmillServApplication' % environ['reconstructed_url'])
+            logger.debug('dispatching request %s to WindmillServApplication' % environ['reconstructed_url'])
             return self.windmill_serv_app(environ, start_response)
         elif environ['PATH_INFO'].find('/windmill-jsonrpc/') is not -1:
-            self.logger.debug('dispatching request %s to WindmillJSONRPCApplication' % environ['reconstructed_url'])
+            logger.debug('dispatching request %s to WindmillJSONRPCApplication' % environ['reconstructed_url'])
             return self.windmill_jsonrpc_app(environ, start_response)
         elif environ['PATH_INFO'].find('/windmill-xmlrpc/') is not -1:
-            self.logger.debug('dispatching request %s to WindmillXMLRPCApplication' % environ['reconstructed_url'])
+            logger.debug('dispatching request %s to WindmillXMLRPCApplication' % environ['reconstructed_url'])
             return self.windmill_xmlrpc_app(environ, start_response)
         else:
-            self.logger.debug('dispatching request %s to WindmillProxyApplication' % reconstruct_url(environ))
+            logger.debug('dispatching request %s to WindmillProxyApplication' % reconstruct_url(environ))
             return self.windmill_proxy_app(environ, start_response)
             
     def __call__(self, environ, start_response):
@@ -347,19 +136,17 @@ def make_windmill_server(http_port=None, js_path=None):
     xmlrpc_methods_instance = convergence.XMLRPCMethods(queue, test_resolution_suite, command_resolution_suite)
     jsonrpc_methods_instance = convergence.JSONRPCMethods(queue, test_resolution_suite, command_resolution_suite)
     
-    windmill_serv_app = WindmillServApplication(logger=logging.getLogger('server.serv'), js_path=js_path)
-    windmill_proxy_app = WindmillProxyApplication(logger=logging.getLogger('server.proxy'))
-    windmill_xmlrpc_app =  WindmillXMLRPCApplication(xmlrpc_methods_instance, logger=logging.getLogger('server.xmlrpc'))
-    windmill_jsonrpc_app = WindmillJSONRPCApplication(jsonrpc_methods_instance,
-                                                      logger=logging.getLogger('server.jsonrpc'))
+    windmill_serv_app = wsgi_fileserver.WSGIFileServerApplication(root_path=js_path, mount_point='/windmill-serv/')
+    windmill_proxy_app = WindmillProxyApplication()
+    windmill_xmlrpc_app =  wsgi_xmlrpc.WSGIXMLRPCApplication(instance=xmlrpc_methods_instance)
+    windmill_jsonrpc_app = wsgi_jsonrpc.WSGIJSONRPCApplication(instance=jsonrpc_methods_instance)
     windmill_chooser_app = WindmillChooserApplication(windmill_serv_app, windmill_jsonrpc_app,
-                                                      windmill_xmlrpc_app, windmill_proxy_app,
-                                                      logger=logging.getLogger('server.chooser'))
-    
+                                                      windmill_xmlrpc_app, windmill_proxy_app)
     
     import cherrypy
-    httpd =  cherrypy.wsgiserver.CherryPyWSGIServer(('', http_port), windmill_chooser_app, server_name='windmill-http')
+    httpd = cherrypy.wsgiserver.CherryPyWSGIServer(('', http_port), windmill_chooser_app, server_name='windmill-http')
 
+    # Attach some objects to httpd for convenience
     httpd.controller_queue = queue
     httpd.test_resolution_suite = test_resolution_suite
     httpd.command_resolution_suite = command_resolution_suite
