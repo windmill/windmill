234ef06d8dcbedd09915750047a5eb63ab5a6337
Mike Taylor <bear@code-bear.com>
2007-07-30 21:47:45 +0000

    Fixed subtle logic issue in the routine that collected files to include in the APP builds.  .svn check was being performed on the variable *after* it had been made into a full path. Also tweaked a bunch of extra whitespace issues.
    git-svn-id: http://svn.getwindmill.com/trunk@286 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import sys, os, copy
 
 APP = 'windmill/bin/windmill-admin.py'
 DATA_FILES = ['windmill']
-              
+
 mikeals_build = ['/Users/mikeal/tmp/CherryPy-3.0.1/cherrypy',
                  '/Users/mikeal/tmp/dateutil-1.1/dateutil',
                  '/Users/mikeal/tmp/simplejson-1.7.1/simplejson',
@@ -29,19 +29,22 @@ mikeals_build = ['/Users/mikeal/tmp/CherryPy-3.0.1/cherrypy',
                  '/Library/Frameworks/Python.framework/Versions/Current/lib/python2.5/email',
                  '/Library/Frameworks/Python.framework/Versions/2.5/lib/python2.5/lib-dynload/', '/Library/Frameworks/Python.framework/Versions/Current/lib/python2.5/site-packages/wx-2.8-mac-unicode/wx',
                  ]
-                
+
 if os.path.isdir('/Users/mikeal'):
     DATA_FILES.extend(mikeals_build)
 
 for filename in copy.copy(DATA_FILES):
-    def add_subfilename(sub_filename):
-        for sub in os.listdir(sub_filename):
-            sub = os.path.join(sub_filename, sub)
-            if os.path.isdir(sub) and not sub.startswith('.') and sub not in DATA_FILES:
+    def add_subfilename(sub_filepath):
+        for item in os.listdir(sub_filepath):
+            sub = os.path.join(sub_filepath, item)
+
+            if os.path.isdir(sub) and not item.startswith('.') and sub not in DATA_FILES:
                 add_subfilename(sub)
-        DATA_FILES.append(sub_filename)
+
+        DATA_FILES.append(sub_filepath)
+
     add_subfilename(filename)
-    
+
 if sys.platform == 'darwin':
     extra_options = dict(setup_requires=['py2app'],
                          app=[APP],
@@ -59,21 +62,22 @@ else:
                          # and install the main script as such
                          scripts=[APP],
                          )
-              
+
 if 'develop' in sys.argv or 'upload' in sys.argv:
     extra_options['install_requires'] = ['cherrypy >= 3.0.1',
-                                            'simplejson',
-                                            'dateutil',
-                                            # All these wsgi_ libraries used to be part of 
-                                            # windmill but are now seperate libraries.
-                                            'wsgi_proxy', 
-                                            'wsgi_jsonrpc',
-                                            'wsgi_xmlrpc',
-                                            'wsgi_fileserver',
-                                            ]
+                                         'simplejson',
+                                         'dateutil',
+                                         # All these wsgi_ libraries used to be part of
+                                         # windmill but are now seperate libraries.
+                                         'wsgi_proxy',
+                                         'wsgi_jsonrpc',
+                                         'wsgi_xmlrpc',
+                                         'wsgi_fileserver',
+                                        ]
 
 setup(
     name='windmill',
     data_files=DATA_FILES,
     packages=['windmill'],
     **extra_options)
+
