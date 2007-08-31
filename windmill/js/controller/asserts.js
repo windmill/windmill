fda21510f53ea153030e202c02a93f10c7fe380a
Matthew Eernisse <mde@fleegix.org>
2007-08-31 07:50:38 +0000

    JSUnit-compatible asserts.
    git-svn-id: http://svn.getwindmill.com/trunk@353 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- windmill/js/controller/asserts.js
+++ windmill/js/controller/asserts.js
@@ -1,3 +1,101 @@
+/*
+Copyright 2006, Open Source Applications Foundation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+windmill.controller.asserts.assertRegistry = {
+  'assertTrue': {
+    expr: function (a) {
+      if (typeof a != 'boolean') {
+        throw('Bad argument to assertTrue.');
+      }
+      return a === true;
+    },
+    errMsg: 'expected true but was false.'
+  },
+
+  'assertFalse': {
+    expr: function (a) {
+      if (typeof a != 'boolean') {
+        throw('Bad argument to assertFalse.');
+      }
+      return a === false;
+    },
+    errMsg: 'expected false but was true.'
+  },
+
+  'assertEquals': {
+    expr: function (a, b) { return a === b; },
+    errMsg: 'expected $1 but was $2.'
+  },
+
+  'assertNotEquals': {
+    expr: function (a, b) { return a !== b; },
+    errMsg: 'expected one of the two values not to be $1.'
+  },
+
+  'assertNull': {
+    expr: function (a) { return a === null; },
+    errMsg: 'expected to be null but was $1.'
+  },
+
+  'assertNotNull': {
+    expr: function (a) { return a !== null; },
+    errMsg: 'expected not to be null but was null.'
+  },
+
+  'assertUndefined': {
+    expr: function (a) { return typeof a == 'undefined'; },
+    errMsg: 'expected to be undefined but was $1.'
+  },
+
+  'assertNotUndefined': {
+    expr: function (a) { return typeof a != 'undefined'; },
+    errMsg: 'expected not to be undefined but was undefined.'
+  },
+
+  'assertNaN': {
+    expr: function (a) { return typeof a == 'undefined'; },
+    errMsg: 'expected to be undefined but was $1.'
+  },
+
+  'assertNotNaN': {
+    expr: function (a) { return typeof a != 'undefined'; },
+    errMsg: 'expected not to be undefined but was undefined.'
+  },
+
+  'assertEvaluatesToTrue': {
+    expr: function (a) { return !!a; },
+    errMsg: 'value of $1 does not evaluate to true.'
+  },
+
+  'assertEvaluatesToFalse': {
+    expr: function (a) { return !a; },
+    errMsg: 'value of $1 does not evaluate to false.'
+  },
+
+  'assertContains': {
+    expr: function (a, b) {
+      if (typeof a != 'string' || typeof b != 'string') {
+        throw('Bad argument to assertContains.');
+      }
+      return (a.indexOf(b) > -1);
+    },
+    errMsg: 'value of $1 does not contain $2.'
+  }
+};
+
 //Currently only does one level below the provided div
 //To make it more thorough it needs recursion to be implemented later
 windmill.controller.asserts.assertText = function (param_object) { 
@@ -103,5 +201,83 @@ windmill.controller.asserts.assertImageLoaded = function (param_object) {
     ret = true;
   }
   return ret;
+};
+
+windmill.controller.asserts.AssertFactory = new function () {
+  var _this = this;
+  function validateArgs(count, args) {
+    if (!(args.length == count ||
+      (args.length == count + 1 && typeof(args[0]) == 'string') )) {
+      throw('Incorrect arguments passed to assert function');
+    }
+  }
+  function createErrMsg(msg, arr) {
+    var str = msg;
+    for (var i = 0; i < arr.length; i++) {
+      var val = arr[i];
+      var display = '<' + val.toString().replace(/\n/g, '') +
+        '> (' + getTypeDetails(val) + ')';
+      str = str.replace('$' + (i + 1).toString(), display);
+    }
+    return str;
+  }
+  function getTypeDetails(val) {
+    var r = typeof val;
+    try {
+      if (r == 'object' || r == 'function') {
+        var m = val.constructor.toString().match(/function\s*([^( ]+)\(/);
+        if (m) { r = m[1]; }
+        else { r = 'Unknown Data Type' }
+      }
+    }
+    finally {
+      r = r.substr(0, 1).toUpperCase() + r.substr(1);
+      return r;
+    }
+  }
+  this.createAssert = function (meth) {
+    return function () {
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift(meth);
+      return _this.doAssert.apply(_this, args);
+    }
+  }
+  this.doAssert = function () {
+    // Convert arguments to real Array
+    var args = Array.prototype.slice.call(arguments);
+    // The actual assert method, e.g, 'equals'
+    var meth = args.shift();
+    // The assert object
+    var asrt = windmill.controller.asserts.assertRegistry[meth];
+    // The assert expresion
+    var expr = asrt.expr;
+    // Validate the args passed
+    var valid = validateArgs(expr.length, args);
+    // Pull off additional comment which may be first arg
+    var comment = args.length > expr.length ?
+      args.shift() : null;
+    // Run the assert
+    var res = expr.apply(window, args);
+    if (res) {
+      return true;
+    }
+    else {
+      var message = meth + ' -- ' +
+        createErrMsg(asrt.errMsg, args);
+      //throw new windmill.controller.asserts.WindmillAssertException(comment, message);
+      return false;
+    }
+  };
+};
+
+// Create all the assert methods on windmill.controller.asserts
+// Using the items in the assertRegistry
+for (var meth in windmill.controller.asserts.assertRegistry) {
+  windmill.controller.asserts[meth] = windmill.controller.asserts.AssertFactory.createAssert(meth);
+}
+
+windmill.controller.asserts.WindmillAssertException = function (comment, message) {
+    this.comment = comment;
+    this.message = message;
 };
 
