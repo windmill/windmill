0dabb4c303b4909a96c7865be075b5f1dbe4e68f
Matthew Eernisse <mde@fleegix.org>
2007-12-21 05:28:00 +0000

    Removed trailing comma in object literal that breaks in IE.
    git-svn-id: http://svn.getwindmill.com/trunk@638 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- test/test_live/jstests/shared/test_nav_windmill_book.js
+++ test/test_live/jstests/shared/test_nav_windmill_book.js
@@ -1,10 +1,9 @@
 
 windmillHomeTest.shared.test_navWindmillBook = new function () {
   this.test_navigate = [
-    { method: "open", params: { url: "http://windmill.osafoundation.org/trac/wiki/WindmillBook" } },
+    { method: "open", params: { url: "http://windmill.osafoundation.org/trac/wiki/WindmillBook" } }
   ];
   this.test_hasNavigated = [
-    { method: "waits.sleep", params: { milliseconds : 2000 } },
     { method: "waits.forElement", params: { id: "WindmillBook" } }
   ];
 };
