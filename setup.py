74c50e1f5c01c834dfac7d2ada7252217ee4a1b9
Mikeal Rogers <mikeal.rogers@gmail.com>
2008-01-17 04:44:05 +0000

    Rolling version.
    -Mikeal
    
    
    
    git-svn-id: http://svn.getwindmill.com/trunk@676 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 import os
 
 PACKAGE_NAME = "windmill"
-PACKAGE_VERSION = "0.3.7pre"
+PACKAGE_VERSION = "0.3.8pre"
 
 SUMMARY = 'Web testing framework intended for complete automation of user interface testing, with strong test debugging and recording capabilities.'
 
