34439b6bb555e8c816c5b805bf757ac1911ff591
Mikeal Rogers <mikeal.rogers@gmail.com>
2008-10-11 03:47:56 +0000

    Removing this revision.
    git-svn-id: http://svn.getwindmill.com/trunk@900 78c7df6f-8922-0410-bcd3-9426b1ad491b

---  windmill/easy_install-2.5-script.py
+++
@@ -1,9 +0,0 @@
-#!C:\Python25\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.6c9','console_scripts','easy_install-2.5'
-__requires__ = 'setuptools==0.6c9'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('setuptools==0.6c9', 'console_scripts', 'easy_install-2.5')()
-)
