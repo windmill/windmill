f111a366bff72f46fb923bf2616016a740df7338
Mikeal Rogers <mikeal.rogers@gmail.com>
2008-10-14 02:01:33 +0000

    Adding functest runner for js tests.
    git-svn-id: http://svn.getwindmill.com/trunk@915 78c7df6f-8922-0410-bcd3-9426b1ad491b

---
+++ test/test_jstests.py
@@ -0,0 +1,14 @@
+
+import sys, os, commands
+
+import functest
+from time import sleep
+
+def test_functest_google_firefox_url():
+    base_directory = os.path.abspath(os.path.dirname(__file__))
+    test_directory = os.path.join(base_directory, 'jstests')
+    
+    output = commands.getoutput('cd '+base_directory+' && windmill firefox -e http://www.getwindmill.com jsdir='+test_directory)
+    print output
+    assert 'ERROR' not in output
+    sleep(.25)
