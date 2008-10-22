40ee8fd8bf77a8e67db7417b783d4b04dd49b0dd
Mikeal Rogers <mikeal.rogers@gmail.com>
2008-10-22 06:14:52 +0000

    More fixes for Safari on windows.
    git-svn-id: http://svn.getwindmill.com/trunk@929 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- windmill/browser/safari.py
+++ windmill/browser/safari.py
@@ -73,7 +73,6 @@ class Safari(object):
 	
 	def __init__(self):
 	    self.netsetup_binary = windmill.settings['NETWORKSETUP_BINARY']
-	    self.safari_binary = windmill.settings['SAFARI_BINARY']
 	    self.test_url = windmill.settings['TEST_URL']
 	
 	def create_redirect(self):
@@ -85,6 +84,7 @@ class Safari(object):
 	    
 	def set_proxy_mac(self):
 	    """Set local Proxy"""
+	    self.safari_binary = windmill.settings['SAFARI_BINARY']
 	    interface_name = find_default_interface_name()
 	    uri = urlparse.urlparse(self.test_url)
 	    set_proxy_command = ' '.join([ self.netsetup_binary, 
