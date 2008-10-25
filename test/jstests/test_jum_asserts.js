16fb8f180b14392880467ebb1bd5ea989dc1590c
Matthew Eernisse <mde@fleegix.org>
2008-10-25 07:31:08 +0000

    Removing all the old JS test unit tests.
    git-svn-id: http://svn.getwindmill.com/trunk@960 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- test/jstests/test_jum_asserts.js
+++
@@ -1,7 +0,0 @@
-windmill.jsTest.require('shared/test_nav_windmill_home.js');
-
-windmillHomeTest.test_jumAsserts = new function () {
-  // Make sure we're back at the home page
-  this.setup =  windmillHomeTest.shared.test_navWindmillHome;
-};
-
--- test/jstests/test_reload_test_code.js
+++
@@ -1,8 +0,0 @@
-windmill.jsTest.require('shared/test_nav_windmill_book.js');
-
-windmillHomeTest.test_reloadTestCode = new function () {
-  // Make sure we're back at the home page
-  this.setup =  windmillHomeTest.shared.test_navWindmillBook;  
-};
-
-
