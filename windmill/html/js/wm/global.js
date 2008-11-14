0c3796fa9156129961ff3a1792c4a3aabdc185a2
Adam Christian <adam.christian@gmail.com>
2008-11-14 10:49:12 +0000

    Replacing the replaceAll helper function with one that doesn't create an infinite loop and kill javascript
    git-svn-id: http://svn.getwindmill.com/trunk@1018 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- windmill/html/js/wm/global.js
+++ windmill/html/js/wm/global.js
@@ -395,11 +395,23 @@ windmill.helpers.normalizeSpaces = function(text)
     }
 }
 
-windmill.helpers.replaceAll = function(text, oldText, newText) {
-    while (text.indexOf(oldText) != -1) {
-	    text = text.replace(oldText, newText);
-    }
-    return text;
+// windmill.helpers.replaceAll = function(text, oldText, newText) {
+//     while (text.indexOf(oldText) != -1) {
+//      text = text.replace(oldText, newText);
+//     }
+//     return text;
+// }
+
+windmill.helpers.replaceAll = function (OldString, FindString, ReplaceString) {
+  var SearchIndex = 0;
+  var NewString = ""; 
+  while (OldString.indexOf(FindString,SearchIndex) != -1)    {
+    NewString += OldString.substring(SearchIndex,OldString.indexOf(FindString,SearchIndex));
+    NewString += ReplaceString;
+    SearchIndex = (OldString.indexOf(FindString,SearchIndex) + FindString.length);         
+  }
+  NewString += OldString.substring(SearchIndex,OldString.length);
+  return NewString;
 }
 
 /**
