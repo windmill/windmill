9f75c1109296c136e85f62b7ebc90aa5761e05eb
Adam Christian <adam.christian@gmail.com>
2009-01-05 21:13:23 +0000

    js tests now run against safari if on mac
    git-svn-id: http://svn.getwindmill.com/trunk@1092 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- test/test_jstests.py
+++ test/test_jstests.py
@@ -12,3 +12,13 @@ def test_functest_google_firefox_url():
     print output
     assert 'ERROR' not in output
     sleep(.25)
+    
+if sys.platform == 'darwin':
+    def test_functest_google_safari_url():
+        base_directory = os.path.abspath(os.path.dirname(__file__))
+        test_directory = os.path.join(base_directory, 'jstests')
+
+        output = commands.getoutput('cd '+base_directory+' && windmill safari -e http://www.getwindmill.com jsdir='+test_directory)
+        print output
+        assert 'ERROR' not in output
+        sleep(.25)
