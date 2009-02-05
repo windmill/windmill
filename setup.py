d12673f0af1c1b4be37373139ce87a3df52be025
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-02-05 21:26:08 +0000

    Preparing for RC1
    git-svn-id: http://svn.getwindmill.com/trunk@1113 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 import os, sys
 
 PACKAGE_NAME = "windmill"
-PACKAGE_VERSION = "1.0beta2"
+PACKAGE_VERSION = "1.0rc1"
 
 SUMMARY = 'Web testing framework intended for complete automation of user interface testing, with strong test debugging and recording capabilities.'
 
