f85999fac5ba494f9bba00145afad41a2d8ebab6
Adam Christian <adam.christian@gmail.com>
2009-05-20 22:34:54 +0000

    great fix for the variable handler allowing for multiple variables within an action
    git-svn-id: http://svn.getwindmill.com/trunk@1258 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- windmill/html/js/wm/global.js
+++ windmill/html/js/wm/global.js
@@ -107,11 +107,11 @@ var handleVariable = function (val){
     for (var i = 0; i < matches.length; i++){
       var m = matches[i];
       if (windmill.varRegistry.hasKey(m)){
-        ret = windmill.helpers.replaceAll(val, m, windmill.varRegistry.getByKey(m));
+        ret = windmill.helpers.replaceAll(ret, m, windmill.varRegistry.getByKey(m));
       }
       //if it doesn't exist and contains the string random we create it (username or pass etc)
       else if (m.indexOf('random') != -1){
-        ret = val.replace(m, windmill.varRegistry.addItemCreateValue(m));
+        ret = ret.replace(m, windmill.varRegistry.addItemCreateValue(m));
       }
     }
   }
