3233e9e8c0fe9a7c4f4d8247a61fbec773c56589
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-06-04 06:22:15 +0000

    Making path to wsgi fileserver abspath.
    git-svn-id: http://svn.getwindmill.com/trunk@1296 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- test/local_tests/__init__.py
+++ test/local_tests/__init__.py
@@ -22,7 +22,7 @@ import wsgi_fileserver
 def setup_module(module):
     windmill.authoring.setup_module(module)
 
-    application = wsgi_fileserver.WSGIFileServerApplication(root_path=os.path.dirname(__file__), mount_point='/windmill-unittests/')
+    application = wsgi_fileserver.WSGIFileServerApplication(root_path=os.path.abspath(os.path.dirname(__file__)), mount_point='/windmill-unittests/')
     windmill.server.wsgi.add_namespace('windmill-unittests', application)
     
 from windmill.authoring import teardown_module
