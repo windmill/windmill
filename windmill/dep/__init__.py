80a3226a33d950aedfc15cdfae4f54034e043f35
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-08-18 23:08:47 +0000

    Fixing import ordering.
    git-svn-id: http://svn.getwindmill.com/trunk@1383 78c7df6f-8922-0410-bcd3-9426b1ad491b

--- windmill/dep/__init__.py
+++ windmill/dep/__init__.py
@@ -1,5 +1,5 @@
-import _mozrunner as mozrunner
 import _simplesettings as simplesettings
+import _mozrunner as mozrunner
 import _wsgi_fileserver as wsgi_fileserver
 import _wsgi_jsonrpc as wsgi_jsonrpc
 import _wsgi_xmlrpc as wsgi_xmlrpc
