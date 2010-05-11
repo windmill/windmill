fb5a73e696cb76e713ff0e8b7144a179561f05b2
Martin Häcker <mhaecker@cs.tu-berlin.de>
2010-05-11 17:47:36 +0800

    prefering locally installed mozrunner1

--- windmill/dep/__init__.py
+++ windmill/dep/__init__.py
@@ -11,8 +11,16 @@ try:
 except:
     import _uuid as uuid
 
+# Prefer native version if available
+try:
+    # To check that it's a mozrunner 1.x based version, 2.x doesn't have global_settings anymore
+    import mozrunner.global_settings
+    import mozrunner
+except:
+    import _mozrunner as mozrunner
+
+
 import _simplesettings as simplesettings
-import _mozrunner as mozrunner
 import _wsgi_fileserver as wsgi_fileserver
 import _wsgi_jsonrpc as wsgi_jsonrpc
 import _wsgi_xmlrpc as wsgi_xmlrpc
